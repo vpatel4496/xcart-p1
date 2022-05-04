992d32f1ef83aeb61a4672085925d6cc39f36f75
Vimal Patel <vpatel4496@gmail.com>
2022-05-04 23:03:14 +0100

    xcart-p1

---
+++ src/main/java/com/x_cart/mobile/pages/Sale.java
@@ -0,0 +1,6 @@
+package com.x_cart.mobile.pages;
+
+import com.sun.org.apache.bcel.internal.classfile.Utility;
+
+public class Sale extends Utility {
+}
---
+++ src/test/java/com/x_cart/mobile/testbase/TestBase.java
@@ -0,0 +1,19 @@
+package com.x_cart.mobile.testbase;
+
+import com.x_cart.mobile.propertyreaders.PropertyReader;
+import com.x_cart.mobile.utility.Utility;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase extends Utility {
+        String browser= PropertyReader.getInstance().getProperty("browser");
+        @BeforeMethod
+        public void setUp(){
+            selectBrowser(browser);
+        }
+        @AfterMethod
+        public void tearDown(){
+            //closeBrowser();
+        }
+}
+
